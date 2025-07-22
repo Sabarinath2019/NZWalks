692174dd02aa6ac6d440f3b32f0a241cfb6650f0
Sabarinath.G <Sabarijiva@gmail.com>
2025-07-22 08:04:38 +0530

    End of section

---
+++ NZWalks/NZWalks.API/Models/Walk.cs
@@ -0,0 +1,14 @@
+﻿namespace NZWalks.API.Models
+{
+    public class Walk
+    {
+        public Guid id { get; set; }
+        public string name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+    }
+}
